0d144212f060304556baae6fd469ff81101e67dd
shivamvora <shivampatel27102001@gmail.com>
2021-02-24 22:29:09 +0530

    materialui add

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Shivam vora is here</h1>
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

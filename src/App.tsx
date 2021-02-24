3e6d1e720c4dff8d9025184f2456dbe6a8c0fbcf
shivamvora <shivampatel27102001@gmail.com>
2021-02-24 23:23:55 +0530

    add appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>      
      </div>
   );
 }
